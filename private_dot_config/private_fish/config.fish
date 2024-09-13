48d4a345ca0c48bccb2227aaf2df83dcb03458a4
MagentaManifold <17zhaomingyuan@gmail.com>
2024-09-13 12:11:37 -0400

    add more programs

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,21 @@
+function fish_greeting
+    if test $(tput cols) -ge 150
+        fastfetch
+    else
+        echo "Hi, Mingyuan!"
+    end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    # oh-my-posh init fish --config ~/.config/fish/themes/catppuccin_macchiato.omp.json | source
+    starship init fish | source
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/mingyuan/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
